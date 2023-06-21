f54eda3a39adf826bb3ca939351668c91d0f7193
Larry-Larriee <lle114@student.hbuhsd.edu>
2023-06-21 13:48:11 -0700

    Cannot Deploy Without Blaze Plan
    Purpose of this project was to create a discord-roblox status using firebase as a REST API middleman. However, this cannot be completed without using the paid plan to enable artifactregistry.googleapis.com, which allows for cloud functions and google's version of REST API

--- functions/index.js
+++ functions/index.js
@@ -1,62 +1,23 @@
-const functions = require('firebase-functions');
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-exports.bigben = functions.https.onRequest((req, res) => {
-  const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr;
-  res.status(200).send(`<!doctype html>
-    <head>
-      <title>Time</title>
-    </head>
-    <body>
-      ${'BONG '.repeat(hours)}
-    </body>
-  </html>`);
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// https request 1
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+
+    
+
 });
-
-const rpc = require("discord-rpc");
-const client = new rpc.Client({ transport: 'ipc' });
-const config = require('./config.json');
-
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-
-    console.log(req);
-
-    client.login({ clientId: config.ClientID }).catch(console.error);
-
-    client.on('ready', () => {
-        console.log('[DEBUG] Presence now active!')
-        console.log('[WARN] Do not close this Console as it will terminate the rpc')
-        console.log('=================== Error Output ===================')
-        client.request('SET_ACTIVITY', {
-            pid: process.pid,
-            activity: {
-                details: config.Details,
-                state: config.State,
-                timestamps: {
-                    start: Date.now()
-                },
-                assets: {
-                    large_image: config.LargeImage,
-                    large_text: config.LargeImageText,
-                    small_image: config.SmallImage,
-                    small_text: config.SmallImageText,
-                },
-                // buttons: [{
-                //         label: config.Button1,
-                //         url: config.Url1
-                //     },
-                //     {
-                //         label: config.Button2,
-                //         url: config.Url2
-                //     },
-                    //labels are the buttons that you wanna provide to your rich presence and urls are the links that leads you when someone press that button
-                    //Note the button won't work for you but don't worry it work for others
-                // ]
-            }
-        })
-    })
-});
-
-app.listen(5000);
